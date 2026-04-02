d704610b4815c464d8ad9300a6fa5d0710fcdfab
Mohamed El Habib El Fekih <habib.elfekih2004@gmail.com>
2026-04-02 11:13:32 +0100

    restyling and rewrite the css/html code for login and nav bars

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from './components/side-bar-component/side-bar-component';
 import { TopNavComponent } from './components/top-nav-component/top-nav-component';
 import { DocPage } from './pages/doc-page/doc-page';
+import { LoginPage } from './pages/login-page/login-page';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SideBarComponent,TopNavComponent,DocPage],
+  imports: [RouterOutlet,SideBarComponent,TopNavComponent,DocPage,LoginPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

02950f29625d84c00f631bb4aa7717f72c720e65
Mohamed El Habib El Fekih <habib.elfekih2004@gmail.com>
2026-04-04 05:24:50 +0100

    add the sign up and finish the doc page + some restyling

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,10 @@ import { SideBarComponent } from './components/side-bar-component/side-bar-compo
 import { TopNavComponent } from './components/top-nav-component/top-nav-component';
 import { DocPage } from './pages/doc-page/doc-page';
 import { LoginPage } from './pages/login-page/login-page';
+import { SignUp } from './pages/sign-up/sign-up';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SideBarComponent,TopNavComponent,DocPage,LoginPage],
+  imports: [RouterOutlet,SideBarComponent,TopNavComponent,DocPage,LoginPage,SignUp],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
